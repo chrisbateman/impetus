78459c321c277219f66776ccaa7ccab5e2d4c137
Chris Bateman <chrisb808@gmail.com>
2014-03-04 23:45:41 -0500

    added package.json, grunt for minification

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                src: 'impetus.js',
+                dest: 'impetus.min.js'
+            },
+            options: {
+                banner: '/**\n' +
+                    ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                    ' * <%= pkg.homepage %>\n' +
+                    ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                    ' * Licensed <%= pkg.licenses.type %> <<%= pkg.licenses.url %>>\n' +
+                    ' */\n'
+            }
+        }
+    
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask('default', ['uglify']);
+    
+};
